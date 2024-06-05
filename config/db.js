954fd78a05ac0e0e6d7bb8e460769bb63e9ca2d4
Zubin Juárez <zubinxxi@gmail.com>
2024-06-05 15:06:29 -0500

    se establece conexión a base de datos mongodb.

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+require('dotenv').config({path:'variables.env'})
+
+mongoose.connect(process.env.DATABASE)
+
+mongoose.connection.on('error', (error) => {
+    console.log(error)
+})
+
+// importar modelos
+require('../models/Vacantes')
