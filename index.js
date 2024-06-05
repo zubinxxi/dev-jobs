5372129617364fb0c0f02cd5dac5a265ec3f5675
Zubin Juárez <zubinxxi@gmail.com>
2024-06-05 15:06:58 -0500

    se agrega código para la conexión a mongodb.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose')
+require('./config/db')
+
 const express = require('express')
 const exphbs = require('express-handlebars')
 const path = require('path')
 const router = require('./routes')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+
+require('dotenv').config({path:'variables.env'})
 
 //Crear la app
 const app = express();
@@ -19,6 +28,18 @@ app.set('view engine', 'handlebars');
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser())
+
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        client: mongoose.connection.getClient()
+    })
+}))
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
