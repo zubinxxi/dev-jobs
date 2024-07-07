ed0c06031f896ccfa64225c20219fc5e3c737c98
Zubin Juárez <zubinxxi@gmail.com>
2024-07-07 13:49:27 -0500

    Agregando la ruta para crear vacantes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/homeController') 
+const vacantesControler = require('../controllers/vacantesController')
+const Vacantes = require('../models/Vacantes')
 
 module.exports = () => {
     router.get('/', homeController.mostrarTrabajos)
+
+    //Crear vacantes
+    router.get('/vacantes/nueva', vacantesControler.formularioNuevaVacante)
 
     return router
 }
