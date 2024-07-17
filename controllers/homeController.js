73a829edf03345936c112b5eab0261e9c6e66d24
Zubin Juárez <zubinxxi@gmail.com>
2024-07-16 19:17:24 -0500

    Se agrega código para mostrar las vacantes en la página principal.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,19 @@
-exports.mostrarTrabajos = (req, res) =>{
+const mongoose = require('mongoose')
+const Vacante = mongoose.model('Vacante')
+
+exports.mostrarTrabajos = async (req, res) =>{
+
+    //Consulta que trae las vacantes desde la base de datos.
+    const vacantes = await Vacante.find().lean() //para que funcione en handlebars debe colocar el .lean() a la consulta.
+
+
+    if(!vacantes) return next()
+
     res.render('home', {
         nombrePagina: 'devJobs',
         tagline: 'Encuentra y Pública Trabajos para Desarrolladores Web',
         barra: true,
-        boton: true
+        boton: true,
+        vacantes: vacantes
     })
 }
