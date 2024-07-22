b6402e1a2a86da4c4ad53dcacb138dfacb62ecb3
Zubin Juárez <zubinxxi@gmail.com>
2024-07-21 20:33:34 -0500

    Se agregó express-validator y connect-flash para los mensajes flash de validaciones.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const flash = require('connect-flash')
 
 
 require('dotenv').config({path:'variables.env'})
@@ -19,6 +21,9 @@ const app = express();
 // Habilitar bodyParser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+// Validación de campos
+app.use(expressValidator())
 
 // Habilitar Handlebars como view
 app.engine('handlebars', 
@@ -45,6 +50,15 @@ app.use(session({
         client: mongoose.connection.getClient()
     })
 }))
+
+// Alerta y flash messages
+app.use(flash())
+
+// Crear Middleware
+app.use((req, res, next) =>{
+    res.locals.mesajes = req.flash()
+    next()
+})
 
 app.use('/', router());
 
