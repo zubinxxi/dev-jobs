719c6583646a622ec955c8d9f9bd7875f161a762
Zubin Juárez <zubinxxi@gmail.com>
2024-07-22 13:39:01 -0500

    se agregó código para enviar el error a la alerta y a la vista

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -11,26 +11,57 @@ exports.formCrearCuenta = (req, res) =>{
 
 exports.validarRegistro = (req, res, next) => {
 
-    req.checkBody('nombre', 'El nombre es obligatorio').notEmpty()
+    // Sanitizar
+    req.sanitizeBody('nombre').escape()
+    req.sanitizeBody('email').escape()
+    req.sanitizeBody('password').escape()
+    req.sanitizeBody('confirmar').escape()
 
 
+    // validar
+    req.checkBody('nombre', 'El Nombre es Obligatorio').notEmpty();
+    req.checkBody('email', 'El email debe ser valido').isEmail();
+    req.checkBody('password', 'El password no puede ir vacio').notEmpty();
+    req.checkBody('confirmar', 'Confirmar password no puede ir vacio').notEmpty();
+    req.checkBody('confirmar', 'El password es diferente').equals(req.body.password);
 
-    const errores = req.validationError()
-    console.log(errores)
 
-    return
+    const errores = req.validationErrors();
+    //console.log(errores)
 
+    if(errores){
+        // si hay errores
+        req.flash('error', errores.map(error => error.msg));
+
+        res.render('crear-cuenta', {
+            nombrePagina: 'Crea tu cuenta en devJobs',
+            tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
+            mensajes: req.flash()
+        });
+        return;
+    }
+
+    // Si toda la validación es correcta
+    next()
 }
 
 
-exports.crearUsuario = async (req, res, next) =>{
-
-    const usuario = new Usuarios(req.body)
+exports.crearUsuario = async (req, res, next) => {
     
-    const nuevoUsuario = await usuario.save()
-
-    if(!nuevoUsuario) return next()
-
-    res.redirect('/iniciar-sesion')
-
+    // crear el usuario
+    const usuario = new Usuarios(req.body);
+    
+    try {
+        await usuario.save();
+        res.redirect('/iniciar-sesion');
+    } catch (error) {
+        req.flash('error', error)
+        //res.redirect('/crear-cuenta')
+        res.render('crear-cuenta', {
+            nombrePagina: 'Crea tu cuenta en devJobs',
+            tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
+            mensajes: req.flash()
+        });
+        return;
+    }
 }
