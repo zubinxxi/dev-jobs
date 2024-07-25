b8d4ab69f3020b973167d91bee51fd28e2ccc6e1
Zubin Juárez <zubinxxi@gmail.com>
2024-07-25 10:51:22 -0500

    Se crean 2 funciones, verificarUsuario y mostrarPanel, para mostrar las vacantes que a creado el usuario logueado.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+const Vacante = mongoose.model('Vacante')
 const passport = require('passport')
 
 
@@ -8,3 +10,24 @@ exports.autenticarUsuario = passport.authenticate('local', {
     badRequestMessage: 'Ambos campos son obligatorios'
 })
 
+// Revisar si el usuario está autenticado o no.
+exports.verificarUsuario = (req, res, next) => {
+    // Revisar el usuario
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+
+    // Redirecionar a inicir sesion
+    res.redirect('iniciar-sesion')
+}
+
+exports.mostrarPanel = async (req, res) => {
+    // Consultar el usuario autenticado
+    const vacantes = await Vacante.find({autor: req.user._id}).lean()
+    res.render('administracion', {
+        nombrePagina: 'Panel de Administración',
+        tagline: 'Crea y Administra tus vacantes desde aquí',
+        vacantes:vacantes
+    })
+}
