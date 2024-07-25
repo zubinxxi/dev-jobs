4b70c205b923c37b4245dce30794c172ea8e88c8
Zubin Juárez <zubinxxi@gmail.com>
2024-07-25 10:53:17 -0500

    Se agregan skills a la lista ej: Flet, Kyvi, Fluter.

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,7 +1,7 @@
 module.exports = {
     seleccionarSkills : (seleccionadas = [], opciones) => {
 
-        const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'];
+        const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox', 'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS', 'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP', 'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize', 'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress', 'Flet', 'Flutter', 'Kyvi'];
         let html = '';
         skills.forEach(skill => {
             html += `
